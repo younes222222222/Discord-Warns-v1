267ff4127cfce8bf52384fa1d2f425e7b68e1301
younes222222222 <161246076+younes222222222@users.noreply.github.com>
2025-02-07 12:37:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-clientId: "", // ايدي البوت
-    guildId: "", // ايدي السيرفر
-    token: "" // توكن البوت
+clientId: "1337049513080389664", // ايدي البوت
+    guildId: "1336297993363787826", // ايدي السيرفر
+    token: "MTMzNjMxMzMxMDEzMTM5MjU5Ng.GLpIrZ.F28IYMPzY3BEc_aS3rqz3tizypHYrCBJrty41o" // توكن البوت
 }
